b951e04ba9f71154a8fb3d516d3db56a9e99b6ea
Bassem <Bsaleh103@gmail.com>
2020-04-02 15:25:35 +0200

    Project Completed

--- app/src/main/java/com/londonappbrewery/destini/StoryLine.java
+++ app/src/main/java/com/londonappbrewery/destini/StoryLine.java
@@ -1,37 +1,26 @@
 package com.londonappbrewery.destini;
 
 public class StoryLine {
-    int storyTextId;
-    int topButtonTextId;
-    int bottomButtonTextId;
+    private int storyTextId;
+    private int topButtonTextId;
+    private int bottomButtonTextId;
 
-    public StoryLine(int storyTextId,int topButtonTextId,int bottomButtonTextId){
+    StoryLine(int storyTextId, int topButtonTextId, int bottomButtonTextId){
         this.storyTextId = storyTextId;
         this.topButtonTextId = topButtonTextId;
         this.bottomButtonTextId = bottomButtonTextId;
     }
 
-    public int getStoryTextId() {
+    int getStoryTextId() {
         return storyTextId;
     }
 
-    public void setStoryTextId(int storyTextId) {
-        this.storyTextId = storyTextId;
-    }
 
-    public int getTopButtonTextId() {
+    int getTopButtonTextId() {
         return topButtonTextId;
     }
 
-    public void setTopButtonTextId(int topButtonTextId) {
-        this.topButtonTextId = topButtonTextId;
-    }
-
-    public int getBottomButtonTextId() {
+    int getBottomButtonTextId() {
         return bottomButtonTextId;
-    }
-
-    public void setBottomButtonTextId(int bottomButtonTextId) {
-        this.bottomButtonTextId = bottomButtonTextId;
     }
 }
